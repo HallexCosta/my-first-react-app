ea5296e51a5b1b029ea2342a5e7e284fe5572e9f
HallexCosta <hallex.costa@hotmail.com>
2020-03-25 23:49:08 -0300

    Updates

---
+++ precache-manifest.4199e9fe57a7ffad31d0dd74f5efd28f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20f36f9743404ee2431f94b6a3ec7cc",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "b4a3b0607fb94b3bcd02",
+    "url": "/my-first-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9db970cb986d5474660",
+    "url": "/my-first-react-app/static/js/2.f908b780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.f908b780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a3b0607fb94b3bcd02",
+    "url": "/my-first-react-app/static/js/main.eb3c7024.chunk.js"
+  },
+  {
+    "revision": "feb6ca079623184cd879",
+    "url": "/my-first-react-app/static/js/runtime-main.1cf94478.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
